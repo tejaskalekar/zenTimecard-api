3eec42b8eafc202ad4312f89c996da3260b0f4bd
tejasKalekar <tkkalekar@gmail.com>
2025-09-11 21:45:03 +0530

    Fixed Flask version in requirements.txt

--- app.py
+++ app.py
@@ -116,4 +116,6 @@ def bulk_timecard():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
